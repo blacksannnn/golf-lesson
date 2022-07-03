c68cb40981f2e3e7f7ea1348a8d65e0d3a372a8f
blacksannnn <platinum.black.96@gmail.com>
2022-07-03 12:22:36 +0900

    新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "lessons#index"
-  root to: "users#index"
+  # root to: "users#index"
+  resources :lessons, only: [:new,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
